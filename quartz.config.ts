8904b743ab406419701c6205c0374f85b3b14861
Andre <andreschafft@mailbox.org>
2025-10-27 16:54:26 +0100

    Fixed bug

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "content/projects", "content/assets/literature", "content/assets/private"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
